6c8eca547aa5ef66409fae3cbe4409771aca596a
Your Name <you@example.com>
2023-03-13 14:36:06 +0800

    add  session dependence

--- main.go
+++ main.go
@@ -19,16 +19,8 @@ type CreditCard struct {
 }
 
 func main() {
-	//dsn := "root:Strong@01@tcp(172.31.0.1:3306)/gin?charset=utf8mb4&parseTime=True&loc=Local"
-	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//var user User
-	//db.AutoMigrate(&User{}, &CreditCard{})
-	//db.First(&user)
-	//db.Model(&user).Association("CreditCard").Clear()
 	r := gin.Default()
+
 	//引入抽离出来的路由分组
 	routes.AdminRouterGroup(r)
 	r.Run()
